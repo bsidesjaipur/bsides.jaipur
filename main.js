de100518ab5f5c6334abf84cdd54f9204400a38c
BSides Jaipur <125820566+bsidesjaipur@users.noreply.github.com>
2023-06-19 00:42:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+
+    const launchUnixTimestamp = (Date.now()/1000)+7*24*60*60;
+
+    new FlipDown(launchUnixTimestamp, {
+        theme: "darkNred",
+        headings: ['', '', '', '']
+    }).start();
+}
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("July 22, 2023 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate the days, hours, minutes and seconds left
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown in the HTML
+  document.querySelector(".days").innerHTML = days;
+  document.querySelector(".hours").innerHTML = hours;
+  document.querySelector(".minutes").innerHTML = minutes;
+  document.querySelector(".seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.querySelector(".flip_container").innerHTML = "COUNTDOWN FINISHED";
+  }
+}, 1000);
