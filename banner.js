18c6ec5a0c63844a73eaf25f14e510b1e91497d2
BSides Jaipur <125820566+bsidesjaipur@users.noreply.github.com>
2023-06-19 13:38:53 +0530

    Add files via upload

--- banner.js
+++ banner.js
@@ -1,25 +1,29 @@
-// ---- ---- ---- ---- ---- //
+
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let interval = null;
 
-document.querySelector('.title1').onmouseover = (event) => {
+document.addEventListener('DOMContentLoaded', () => {
+  const title1 = document.querySelector('.title1');
   let iteration = 0;
 
   clearInterval(interval);
 
   interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
+    title1.innerText = title1.innerText
       .split('')
       .map((letter, index) => {
         if (index < iteration) {
-          return event.target.dataset.value[index];
+          return title1.dataset.value[index];
         }
         return letters[Math.floor(Math.random() * 26)];
       })
       .join('');
-    if (iteration >= event.target.dataset.value.length) {
+
+    if (iteration >= title1.dataset.value.length) {
       clearInterval(interval);
     }
+
     iteration += 1 / 3;
-  }, 50);
-};
+  }, 30);
+});
--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 document.getElementById("myVideo").loop = true;
+
+// function sendNotificationOnLoad() {
+  
+//   Swal.fire({
+//     title: '<strong>Use Coupons</strong>',
+//     icon: 'info',
+//     html:
+//       'You can use these coupon code to get <b>discount</b>, </br> Student & Individual : <b>NULLCON</b></br>Professional & Contributor : <b>BSIDESJAIPUR</b>',
+//     showCloseButton: true,
+//     showCancelButton: true,
+//     focusConfirm: false,
+//     confirmButtonText:
+//       '<i class="fa fa-thumbs-up"></i> Great!',
+//     confirmButtonAriaLabel: 'Thumbs up, great!',
+//     cancelButtonText:
+//       '<i class="fa fa-thumbs-down"></i>',
+//     cancelButtonAriaLabel: 'Thumbs down'
+//   })
+// };
+
+  
+  window.addEventListener('load', sendNotificationOnLoad);
+  
+
+Swal.fire({
+    title: '<strong>Use Coupons</strong>',
+    icon: 'info',
+    html:
+      'You can use these coupon codes to get a <b>discount</b>:<br>' +
+      'Student & Individual: <b>NULLCON</b><br>' +
+      'Professional & Contributor: <b>BSIDESJAIPUR</b>',
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfirm: false,
+    confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+    confirmButtonAriaLabel: 'Thumbs up, great!',
+    cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
+    cancelButtonAriaLabel: 'Thumbs down',
+    customClass: {
+      container: 'custom-swal-container',
+      title: 'custom-swal-title',
+      content: 'custom-swal-content',
+      closeButton: 'custom-swal-close-button',
+      confirmButton: 'custom-swal-confirm-button',
+      cancelButton: 'custom-swal-cancel-button'
+    }
+  });
+  
