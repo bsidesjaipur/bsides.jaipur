93a9cb44c3c507ab716662d5623af0aef9b9daed
BSides Jaipur <125820566+bsidesjaipur@users.noreply.github.com>
2023-07-13 23:27:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ window.onload = () => {
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("July 22, 2023 00:00:00").getTime();
+var countDownDate = new Date("September 2, 2023 00:00:00").getTime();
 
 // Update the countdown every 1 second
 var x = setInterval(function() {
